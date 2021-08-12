918dbddf58fe40fe9a6b0943584238ce758551e6
Jimmy <jimhaotra1234@hotmail.com>
2021-08-12 21:43:31 +0200

    Board color, arrays of arrays of divs. Start button. Stones white color, onclick changes to black stone

---
+++ main.js
@@ -0,0 +1,124 @@
+console.log("First line in main.js");
+
+const TILE_STATE_EMPTY = 0;
+const TILE_STATE_BLACK = 1;
+const TILE_STATE_WHITE = 2;
+
+class Tile {
+    constructor(state, x, y) {
+        this.state = state;
+        this.x = x;
+        this.y = y;
+    }
+    isEmpty() {
+        // Return whether this tile is empty (a bool)
+        return this.state == TILE_STATE_EMPTY;
+    }
+    hasBlackPiece() {
+        // Return whether the piece on this tile is black
+        return this.state == TILE_STATE_BLACK;
+    }
+    hasWhitePiece() {
+        return this.state == TILE_STATE_WHITE;
+    }
+}
+
+// Constants
+const BLACK = 0;
+const WHITE = 1;
+
+let isWhoseTurn = BLACK;
+let boardSize = 15;
+
+let board = [];  // Should be a 2D array of Tile
+let tileDivs = [];
+
+function initBoard() {
+    console.log("initBoard()");
+    // Set value of board to a 2D array whose size if boardSize * boardSize
+    for (let i = 0; i < boardSize; ++i) {  // loop rows
+        row = []
+        for (let j = 0; j < boardSize; ++j) {  // loop cols
+            tile = new Tile(TILE_STATE_EMPTY, i, j);
+            row.push(tile);
+            
+            console.log(i.toString() + "," + j.toString());
+        }
+        board.push(row);
+    }
+    
+    console.log(board);
+    drawBoard(); // erase
+}
+
+function drawBoard() {
+    // Loop each tile, and create a corresponding div, then set it as 
+    // child of "game_board" div.
+
+    // Store tile divs in an array for faster access later
+    let tileDivs = [];
+    let gameBoard = document.getElementById("game_board");
+    for(let i = 0; i < boardSize; i++) {
+        for(let j = 0; j < boardSize; j++) {
+            tileDiv = document.createElement("div");
+            tileDiv.setAttribute("class", "tileDivs");
+            tileDiv.style.left = `${10 + j * 35}px`;
+            tileDiv.style.top = `${10 + i * 35}px`;
+            tileDiv.setAttribute("id", "tile: " + i.toString() + "," + j.toString());
+            tileDiv.setAttribute("onclick", `modifyStoneColor(${i}, ${j}, ${2})`);
+            tileDivs.push(tileDiv);
+        }
+    }
+    
+    // Set each tile div as children of "game_board" div.
+    for (tileDiv of tileDivs) {
+        gameBoard.appendChild(tileDiv);
+    }
+}
+
+function testAlert(x) {
+    if (Number.isInteger(x)){
+
+        alert(x)
+    }
+}
+function modifyStoneColor(xtile, ytile, color) {
+    tile = document.getElementById("tile: " + xtile + "," + ytile);
+    // alert(color)
+    // if (color == 1) {
+    //     tile.style.background-color == "white"
+    // }
+    if (color == '2') {
+        // alert("heakjlfksd")}
+        tile.style.backgroundColor = "black";
+    }
+    // }
+    // else {
+    //     tile.style.background-color == "transparent"
+    // }
+}
+
+function nextTurn() {
+    if (isWhoseTurn == BLACK) {
+        isWhoseTurn = WHITE;
+    } else {
+        isWhoseTurn = BLACK;
+    }
+}
+
+function getTileDiv(x, y) {
+    // Return tile div at position (x, y)
+    return tileDivs[x * boardSize + y];
+}
+
+function isWon(lastPiece) {
+    // Check if the given piece participate in a 5-in-a-row
+}
+
+function onClickBoard(x, y) {
+    // This is called when a tile div is clicked
+}
+
+function showBoard() {
+    // Set style of board depending on current pieces on the board
+};
