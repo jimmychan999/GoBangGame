15450457655122fc1099cf177d5bd080442eb171
Donny Chan <chenyingfa1999@qq.com>
2021-08-26 13:46:34 +0800

    UI update, more help content

--- main.js
+++ main.js
@@ -2,12 +2,6 @@
 const EMPTY = 0;
 const BLACK = 1;
 const WHITE = 2;
-const THEME_LIGHT = "light";
-const THEME_DARK = "dark";
-const THEME_HIGH_CONTRAST = "high-contrast"
-
-// Starting parameters
-let curTheme = THEME_LIGHT;
 let isWhoseTurn = BLACK;
 
 let boardNumRows = 15;
@@ -354,33 +348,6 @@ function hideBoardStones() {
         tileDiv.style.boxShadow = "0 0 0 0 rgba(0,0,0,0)";
     }
 }
-
-function initTheme() {
-    let storedTheme = localStorage.getItem("theme");
-    if (storedTheme == null) {
-        curTheme = THEME_LIGHT;
-    } else {
-        curTheme = storedTheme;
-    }
-    setTheme(curTheme);
-}
-
-function setTheme(theme) {
-    localStorage.setItem("theme", theme);
-    document.getElementById("theme-switcher").href = "../css/themes/" + theme + ".css";
-}
-
-function changeTheme() {
-    if (curTheme == THEME_LIGHT) {
-        curTheme = THEME_DARK;
-    } else if (curTheme == THEME_DARK) {
-        curTheme = THEME_HIGH_CONTRAST;
-    } else {
-        curTheme = THEME_LIGHT;
-    }
-    setTheme(curTheme);
-}
-
 
 function clearBoard() {
     setBoardArrayEmpty();
